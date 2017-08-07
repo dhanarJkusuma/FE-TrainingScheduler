693925a546ac19f918cc99492580515dd3958d42
Dhanar J Kusuma <dhanar.januardhi.k@mail.ugm.ac.id>
2017-08-07 11:45:38 +0700

    fix something

--- app/Http/Controllers/JadwalController.php
+++ app/Http/Controllers/JadwalController.php
@@ -190,22 +190,96 @@ class JadwalController extends Controller
      */
     public function update(StoreJadwal $request, $id)
     {
-        $found = Jadwal::where('hari','=', $request->input('hari'))
-                ->where('sesi','=', $request->input('sesi'))
-                ->where('grup_id','=',$request->input('grup_id'))
-                ->where('id', '<>', $id)
-                ->count();
-        if($found > 0){
-            $request->session()->flash('error', "Gagal menambahkan jadwal. Hari, dan sesi sudah digunakan.");
-        }else if($request->input('pelatih_i')==0){
+
+        $trainee = array();
+        $selectedTrainee = array();
+        $group = Group::find($request->input('grup_id'));
+
+        //cek jadwal berdasar lokasi, hari, dan sesi
+        $found = Jadwal::join('grup_latihan', function($join) use ($request, $group){
+            $join->on('jadwal.grup_id','=', 'grup_latihan.id')
+                ->where('grup_latihan.lokasi_latihan_id','=', $group->lokasi_latihan_id)
+                ->where('jadwal.grup_id', '<>', $id)
+                ->where('jadwal.hari', '=', $request->input('hari'))
+                ->where('jadwal.sesi', '=', $request->input('sesi'));
+        })->count();
+
+        //cek ketersediaan pelatih
+        $trainerSchedule =  Jadwal::where('hari','=', $request->input('hari'))
+                                    ->where('sesi', '=', $request->input('sesi'))
+                                    ->where('id', '<>', $id)
+                                    ->where(function($query) use ($request){
+                                        $query->where('pelatih_i','=', $request->input('pelatih_i'))
+                                        ->orWhere('pelatih_ii','=', $request->input('pelatih_i'))
+                                        ->orWhere('pelatih_iii','=', $request->input('pelatih_i'))
+                                        ->orWhere('pelatih_i','=', $request->input('pelatih_ii'))
+                                        ->orWhere('pelatih_ii','=', $request->input('pelatih_ii'))
+                                        ->orWhere('pelatih_iii','=', $request->input('pelatih_ii'))
+                                        ->orWhere('pelatih_i','=', $request->input('pelatih_iii'))
+                                        ->orWhere('pelatih_ii','=', $request->input('pelatih_iii'))
+                                        ->orWhere('pelatih_iii','=', $request->input('pelatih_iii'));
+                                    })->get();
+
+        //memasukkan nama pelatih yang dipilih ke dalam array yang nantinya digunakan untuk membandingkan.
+        if($request->input('pelatih_i')!=0){
+            array_push($selectedTrainee, $request->input('pelatih_i'));
+        }
+        if($request->input('pelatih_ii')!=0){
+            array_push($selectedTrainee, $request->input('pelatih_ii'));
+        }
+        if($request->input('pelatih_iii')!=0){
+            array_push($selectedTrainee, $request->input('pelatih_iii'));
+        }
+
+        //Memasukkan nama-nama pelatih yang tidak tersedia ke dalam array
+        foreach ($trainerSchedule as $t) {
+            if(in_array($t->pelatih_i, $selectedTrainee)){
+                array_push($trainee, $t->pelatih_i);
+            }
+            if(in_array($t->pelatih_ii, $selectedTrainee)){
+                array_push($trainee, $t->pelatih_ii);
+            }
+            if(in_array($t->pelatih_iii, $selectedTrainee)){
+                array_push($trainee, $t->pelatih_iii);
+            }
+            
+        }
+
+        //menghilangkan duplikasi pelatih
+        $trainee = array_unique($trainee);
+
+        //mendapatkan identitas pelatih berdasar id
+        $users = User::where(function($query) use ($trainee){
+            foreach ($trainee as $t) {
+                $query->orWhere('id','=',$t);
+            }
+        })->get();
+
+
+        
+
+        if($request->input('pelatih_i')==0){
             $request->session()->flash('error', "Gagal menambahkan jadwal. Pelatih I wajib untuk diisi.");
+        }else if($found > 0){
+            $request->session()->flash('error', "Gagal menambahkan jadwal. Hari, dan sesi sudah digunakan.");
+        }else if(count($trainerSchedule) > 0){
+            $na = "";
+            foreach ($users as $u) {
+                $na .= "(" . $u->name . ")";
+                $na .= " ";
+            }
+            $request->session()->flash('error', "Gagal menambahkan jadwal. Pelatih dengan nama ". $na ." tidak tersedia pada waktu tersebut.");
+
         }else{
+            //jika lokasi tersedia & pelatih ada 
+            
             $data = $request->except(['_token', '_method']);
             $data['pelatih_ii'] = ($data['pelatih_ii']!=0) ? $data['pelatih_ii'] : null;
             $data['pelatih_iii'] = ($data['pelatih_iii']!=0) ? $data['pelatih_ii'] : null;
             
             Jadwal::find($id)->update($data);
             $request->session()->flash('status','Berhasil mengubah jadwal');
+
         }
 
         return redirect()->route('group-jadwal',['id' => $request->input('grup_id')]);
